bd9bf58e89af94482cb0264bdd2a0dd6d4f1a600
divyam-kalwar <divyamkalwar98@gmail.com>
2026-05-06 23:21:19 +0530

    Add recentlyViewedItem

--- src/ItemClassHierarchy/Inventory.java
+++ src/ItemClassHierarchy/Inventory.java
@@ -5,7 +5,6 @@ import java.util.*;
 public class Inventory <T extends Item>{
     private Map<String, T> items = new HashMap<>();
 
-    // Add item
     public void addItem(T item) {
         if (items.containsKey(item.getId())) {
             throw new IllegalArgumentException("Duplicate ID not allowed");
@@ -13,12 +12,10 @@ public class Inventory <T extends Item>{
         items.put(item.getId(), item);
     }
 
-    // Get item
     public T getItem(String id) {
         return items.get(id);
     }
 
-    // Remove item
     public void removeItem(String id) {
         items.remove(id);
     }
